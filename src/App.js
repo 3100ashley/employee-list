c71c7c83c6b016ef831b843f2a48ca2959ae3a71
Ashley Manese <ashleymanese31@gmail.com>
2022-11-28 11:00:56 -0800

    updated and added new employees with hardcoded data

--- src/App.js
+++ src/App.js
@@ -1,23 +1,96 @@
 
-import './App.css';
+import './index.css';
+import Employee from './components/Employee';
+import {useState} from 'react';
+import {v4 as uuidv4} from 'uuid'
+import AddEmployee from './components/AddEmployee';
+
+
 
 function App() {
+  const [role, setRole] = useState("dev");
+  const [employees, setEmployees] = useState(
+    [
+      {
+        id: 1,
+        name: 'Ashley', 
+        role: 'Dev', 
+        img:"https://images.pexels.com/photos/3831645/pexels-photo-3831645.jpeg" 
+      },
+      {
+        id: 2,
+        name: 'Pam', 
+        role: 'Project Manager', 
+        img:"https://images.pexels.com/photos/3586798/pexels-photo-3586798.jpeg" 
+      },
+      {
+        id: 3,
+        name: 'Steve', 
+        role: 'Manger', 
+        img:"https://images.pexels.com/photos/14440674/pexels-photo-14440674.jpeg" 
+      },
+      {
+        id: 4,
+        name: 'Angela', 
+        role: 'Accountant', 
+        img:"https://images.pexels.com/photos/8837775/pexels-photo-8837775.jpeg" 
+      },
+      {
+        id: 5,
+        name: 'Dwight', 
+        role: 'Assistant Manager', 
+        img:"https://images.pexels.com/photos/25758/pexels-photo.jpg" 
+      },
+    ]
+  )
+
+  function updateEmployee(id, newName, newRole) {
+    const updatedEmployees = employees.map((employee) => {
+        if(id === employee.id){
+          return {...employee, name: newName, role: newRole}
+        }
+        return employee
+    });
+    setEmployees(updatedEmployees)
+  }
+
+  function newEmployee(name, role, img){
+    const newEmployee = {
+      id: uuidv4(),
+      name: name,
+      role: role,
+      img: img
+    }
+    setEmployees([...employees, newEmployee])
+  }
+
+  const showEmployees = true;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App ">
+      {showEmployees ? 
+      <>
+      <input type="text" onChange={(e) => {
+        console.log(e.target.value)
+        setRole(e.target.value);
+      }}/>
+      <div className='flex flex-wrap justify-center'>
+        {employees.map((employee) => {
+          return (
+            <Employee 
+              key={employee.id}
+              id={employee.id}
+              name={employee.name} 
+              role={employee.role} 
+              img={employee.img}
+              updateEmployee={updateEmployee}
+            />
+            )
+        })}
+      </div>
+      <AddEmployee newEmployee={newEmployee}></AddEmployee>
+      </>:
+      <p>you cannot see the employees</p>}
     </div>
   );
 }
