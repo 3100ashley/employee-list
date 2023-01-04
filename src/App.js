54bc30d9a2b21838555184b04781f9ddec1dd412
Ashley Manese <ashleymanese31@gmail.com>
2023-01-03 19:19:13 -0800

    adds customer page and connects to api

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Dictionary from "../src/pages/Dictionary";
 import Definition from "../src/pages/Definition";
 import NotFound from "./components/NotFound"
+import Customers from "./pages/Customer";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Routes>
           <Route path="/employees" element={<Employees />} />
           <Route path="/dictionary" element={<Dictionary />} />
+          <Route path="/customers" element={<Customers />} />
           <Route path="/404" element={<NotFound />} />
           <Route path="/dictionary/:search" element={<Definition />} />
           <Route path="*" element={<NotFound />} />
